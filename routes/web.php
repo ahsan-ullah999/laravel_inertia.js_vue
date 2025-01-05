5f4af852e849f421fe100cd5224a0b15bbbfaa64
Ahsan ullah <ahsanullah0216@gmail.com>
2025-01-05 10:25:19 +0600

    grouping middleware

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,20 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::inertia('/','Home')->name('home');
+//for Authenticated user
+Route::middleware('auth')->group(function(){
+Route::inertia('/dashboard','Dashboard')->name('dashboard');
+Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+});
+//for guest user
+Route::middleware('guest')->group(function(){
 Route::inertia('register','Auth/Register')->name('register');
 Route::post('/register',[AuthController::class, 'register']);
-
-
-
 Route::inertia('login','Auth/Login')->name('login');
 Route::post('/login',[AuthController::class, 'login']);
+});
 
-Route::inertia('/dashboard','Dashboard')->name('dashboard');
 
-
-Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
 
 
 
